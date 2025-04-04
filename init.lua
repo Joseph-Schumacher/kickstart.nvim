003f10caaf5f0085ea9174c2d5facd3b0bb2cd51
Joseph Karl Schumacher <joseph.karl.schumacher.joseph@cern.ch>
2025-04-04 02:43:44 +0200

    switch python to ruff

--- init.lua
+++ init.lua
@@ -687,6 +687,7 @@ require("lazy").setup({
 				clangd = {},
 				gopls = {},
 				pyright = {},
+				ruff = {},
 				rust_analyzer = {},
 				-- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
 				--
@@ -782,7 +783,7 @@ require("lazy").setup({
 			formatters_by_ft = {
 				lua = { "stylua" },
 				-- Conform can also run multiple formatters sequentially
-				python = { "isort", "autopep8" },
+				python = { "ruff_fix", "ruff_format", "ruff_organize_imports" },
 				--
 				-- You can use 'stop_after_first' to run the first available formatter from the list
 				-- javascript = { "prettierd", "prettier", stop_after_first = true },
